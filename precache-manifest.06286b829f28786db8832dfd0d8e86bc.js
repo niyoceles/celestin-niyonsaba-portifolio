07c7b393bddc256c5c559b1656e36043f34e119b
Celestin Niyonsaba <niyoceles3@gmail.com>
2020-10-17 20:10:17 +0200

    Updates

---
+++ precache-manifest.06286b829f28786db8832dfd0d8e86bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90244293c2490018a0aba6bf9582f36",
+    "url": "/celestin-niyonsaba-portifolio/index.html"
+  },
+  {
+    "revision": "4040062b4d05993a32e6",
+    "url": "/celestin-niyonsaba-portifolio/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "79f7650076e9eb6ddd6d",
+    "url": "/celestin-niyonsaba-portifolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4040062b4d05993a32e6",
+    "url": "/celestin-niyonsaba-portifolio/static/js/2.b367bd72.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/celestin-niyonsaba-portifolio/static/js/2.b367bd72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f7650076e9eb6ddd6d",
+    "url": "/celestin-niyonsaba-portifolio/static/js/main.c27c4f68.chunk.js"
+  },
+  {
+    "revision": "eb1d6de0e85b0aa5715f",
+    "url": "/celestin-niyonsaba-portifolio/static/js/runtime-main.3656a613.js"
+  },
+  {
+    "revision": "18dd56a2b0c5d98c1fae7177e60a64a2",
+    "url": "/celestin-niyonsaba-portifolio/static/media/authors-haven.18dd56a2.png"
+  },
+  {
+    "revision": "235a7a936ff1323c5a2d8e25eb43fed3",
+    "url": "/celestin-niyonsaba-portifolio/static/media/celestin-portilio.235a7a93.png"
+  },
+  {
+    "revision": "c89d0046f786426f15e204acf91bb259",
+    "url": "/celestin-niyonsaba-portifolio/static/media/celestin-profile5.c89d0046.jpg"
+  },
+  {
+    "revision": "698622ce14cac6993d640cb9fec1fc3d",
+    "url": "/celestin-niyonsaba-portifolio/static/media/relax-app.698622ce.png"
+  },
+  {
+    "revision": "423d6a95989d95b1a6f3c08489ff21cb",
+    "url": "/celestin-niyonsaba-portifolio/static/media/stylos.423d6a95.png"
+  }
+]);
